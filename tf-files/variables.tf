79820b59a6b73735bf722034c577a92c7507ea76
polat <beratalha29@gmail.com>
2024-08-10 17:53:35 +0200

    proje202 terraform

---
+++ tf-files/variables.tf
@@ -0,0 +1,15 @@
+variable "git-name" {
+  default = "xxxxxxxxx"
+}
+
+variable "git-token" {
+  default = "xxxxxxxxxxxxxx"
+}
+
+variable "key-name" {
+  default = "oliver"
+}
+
+variable "hosted-zone" {
+  default = "clarusway.us"
+}
